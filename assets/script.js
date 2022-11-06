c440b319fad8ac8230d23df76fc1b614f540aa38
Amy Snarr <amy.snarr16@gmail.com>
2022-11-06 13:54:09 -0700

    added error for not selecting at least one criteria

--- assets/script.js
+++ assets/script.js
@@ -2,8 +2,8 @@
 var generateBtn = document.querySelector("#generate");
 
 
-// Generate the password
 
+// Password generator begins here
 const criteria = {
     lowercase: 'abcdefghijklmnopqrstuvwxyz',
     uppercase: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
@@ -14,9 +14,10 @@ const criteria = {
 function generatePassword() {
     var passwordCharSet = "";
 
+// Ask for criteria
     var length = window.prompt("Enter password length (minimum 8, maximum 128)");
         if (length < 8 || length > 128 || isNaN(length) ) {
-            window.confirm("Invalid entry, please try again.")
+            window.alert("Invalid entry, please try again.")
             return;
         }
     var lowercase = window.confirm("Would you like to use LOWERCASE letters?");
@@ -31,11 +32,22 @@ function generatePassword() {
         if (symbols) {
          passwordCharSet += criteria.symbol;
         };
-
     var numbers = window.confirm("Would you like to use NUMBERS?");
         if (numbers) {
             passwordCharSet += criteria.number;
         };
+    if (!lowercase && !uppercase && !symbols && !numbers){
+        window.alert("You must select at least one criteria for your password. Please try again.")
+        return;
+    }
+    window.confirm("Would you like to confirm the following selection?" +
+                "\n\nPassword length: " + length +
+                "\nLowercase: " + lowercase +
+                "\nUppercase: " + uppercase +
+                "\nSymbols: " + symbols +
+                "\nNumbers: " + numbers)
+
+// Generate password
     var password = "";
         for (let i = 0; i < length; i++) {
             password += passwordCharSet[Math.floor(Math.random() * passwordCharSet.length)]
