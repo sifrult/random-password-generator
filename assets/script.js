715c2c40f5b387ca3e6a3f082f897d7c40bc0c7c
Amy Snarr <amy.snarr16@gmail.com>
2022-11-05 20:04:09 -0600

    added variables

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,9 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var numbers = "0123456789"
+var lowerLetters = "abcdefghijklmnopqrstuvwxyz"
+var upperLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+var specialChar = " !#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
 
 // Write password to the #password input
 function writePassword() {
