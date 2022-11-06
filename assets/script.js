cdf9fd2fc25713e3f2c0594aeb766e9185af3fac
Amy Snarr <amy.snarr16@gmail.com>
2022-11-05 23:05:39 -0600

    added prompt confirming criteria selection

--- assets/script.js
+++ assets/script.js
@@ -9,17 +9,12 @@ var specialChar = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
 document.getElementById("generate").onclick = function() {
     window.confirm("Press 'Ok' to begin selecting your password criteria");
 
-
-
     var userInputMinChar = window.prompt ("Enter MINIMUM amount of characters (min of 8)")
     var userMinChar = userInputMinChar;
     if (userMinChar < 8 || userMinChar > 128 || isNaN(userInputMinChar) ) {
         window.confirm("Invalid entry, please try again.")
         return;
     }
-
-
-
 
     var userInputMaxChar = window.prompt ("Enter MAXIMUM amount of characters (max of 128)")
     var userMaxChar = userInputMaxChar;
@@ -28,17 +23,12 @@ document.getElementById("generate").onclick = function() {
         return;
     }
 
-
-
-
     var userInputLowercase = window.prompt ("Type 'yes' if you would like to include LOWERCASE letters, type 'no' if you don't want to include them")
     var userLowercase = userInputLowercase;
     if (userLowercase !== "no" && userLowercase !== "yes") {
         window.confirm("Invalid entry, please try again.")
         return;
     }
-
-
 
     var userInputUppercase = window.prompt ("Type 'yes' if you would like to include UPPERCASE letters, type 'no' if you don't want to include them")
     var userUppercase = userInputUppercase;
@@ -47,16 +37,12 @@ document.getElementById("generate").onclick = function() {
         return;
     }
 
-
-
     var userInputNumeric = window.prompt ("Type 'yes' if you would like to include NUMBERS, type 'no' if you don't want to include them")
     var userNumeric = userInputNumeric;
     if (userNumeric !== "no" && userNumeric !== "yes") {
         window.confirm("Invalid entry, please try again.")
         return;
     }
-
-
 
     var userInputSpecialCharacters = window.prompt ("Type 'yes' if you would like to include SPECIAL CHARACTERS, type 'no' if you don't want to include them")
     var userSpecialCharacters = userInputSpecialCharacters;
@@ -65,10 +51,18 @@ document.getElementById("generate").onclick = function() {
         return;
     }
 
-
     if (userLowercase === "no" && userUppercase === "no" && userNumeric === "no" && userSpecialCharacters === "no") {
         window.confirm("You must say 'yes' to at least one character type. Please make your selections again.")
         return;
+    } else {
+        window.confirm ( "Please confirm your selection:" +
+            "\n\nMin character: " + userMinChar +
+            "\nMax characters: " + userMaxChar +
+            "\nLowercase: " + userLowercase +
+            "\nUppercase: " + userUppercase +
+            "\nNumbers: " + userNumeric +
+            "\nSpecial characters: " + userSpecialCharacters
+        )
     }
 
 }
